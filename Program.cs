8c231639b25f7974fcd6bb9e69fa1be3c9625f22
Pozzame <pozzame@gmail.com>
2024-07-03 11:16:36 +0200

    Commenti

--- Program.cs
+++ Program.cs
@@ -6,32 +6,40 @@ int lanci = 0;
 Console.Clear();
 while (playerUmano >= 0 && playerPC >=0)
 {
-    Console.WriteLine($"Lancio numero: {++lanci}");
+    Console.WriteLine($"Lancio numero: {++lanci}"); //Contatore turno
+
+     //Lancio umano
+    Console.WriteLine("Lancia i dadi");
+    Console.ReadKey();
     int lancio1PUmano = rng.Next(1,7);
     int lancio2PUmano = rng.Next(1,7);
-    Console.WriteLine("Premi invio per lanciare i dadi");
-    Console.ReadLine();
+    int lancioPUmano = lancio1PUmano + lancio2PUmano;
     Console.Clear();
     Console.WriteLine($"Hai fatto: {lancio1PUmano} e {lancio2PUmano}");
-    int lancioPUmano = lancio1PUmano + lancio2PUmano;
-    int lancio1PPC = rng.Next(1,7);
+    //
+
+    //Lancio PC
+    int lancio1PPC = rng.Next(1,7); 
     int lancio2PPC = rng.Next(1,7);
-    Console.WriteLine($"Il PC ha fatto: {lancio1PPC} e {lancio2PPC}");
     int lancioPPC = lancio1PPC + lancio2PPC;
-    if (lancioPUmano < lancioPPC)
+    Console.WriteLine($"Il PC ha fatto: {lancio1PPC} e {lancio2PPC}");
+    //
+
+    if (lancioPUmano < lancioPPC) //Se vince PC
     {
         Console.WriteLine("Il PC vince!");
-        playerUmano -= lancioPPC - lancioPUmano;
+        playerUmano -= lancioPPC - lancioPUmano; //Aggiorno punteggio
     } 
-    else if (lancioPUmano > lancioPPC)
+    else if (lancioPUmano > lancioPPC) //Se vince umano
     {
         Console.WriteLine("Hai vinto!");
-        playerPC -= lancioPUmano - lancioPPC;
+        playerPC -= lancioPUmano - lancioPPC; //Aggiorno punteggio
     }
-    else Console.WriteLine("Pari!");
-    Console.WriteLine($"Punteggio giocatore: {playerUmano}\nPunteggio PC: {playerPC}\n");
+    else Console.WriteLine("Pari!"); //Se pari
+
+    Console.WriteLine($"Punteggio giocatore: {playerUmano}\nPunteggio PC: {playerPC}\n"); //Visualizza punteggio parziale
     Thread.Sleep(500);
 }
-if (playerUmano < playerPC )
+if (playerUmano < playerPC ) //Visualizza punteggio finale
     Console.WriteLine($"Hai perso!\nIl PC ha vinto in {lanci} tiri con un vantaggio di {playerPC-playerUmano} punti!");
     else Console.WriteLine($"Hai vinto in {lanci} tiri con un vantaggio di {playerUmano-playerPC} punti!");
